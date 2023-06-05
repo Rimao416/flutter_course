2c03478df88503694f7096df355b2285e74b5451
omari <omarkayumba12345@gmail.com>
2023-06-05 10:30:02 +0100

    002Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter002/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 78, 13, 151),
+            Color.fromARGB(255, 78, 13, 151)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
+  ));
 }
