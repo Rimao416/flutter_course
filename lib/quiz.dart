ee1b9c8785fdf09a9c3298b14b86326898a30fc5
omari <omarkayumba12345@gmail.com>
2023-06-07 08:13:06 +0100

    NavigationWay2

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -11,16 +11,16 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  var activeScreen = 'start-screen';
   @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // void initState() {
+  //   activeScreen = 'questions-screen';
+  //   super.initState();
+  // }
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -34,7 +34,9 @@ class _QuizState extends State<Quiz> {
               Color.fromARGB(255, 78, 13, 151)
             ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
